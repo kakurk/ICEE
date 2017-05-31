af9813a652dee8dc38f47c3f68d8812042476d59
kkurkela <kyleakurkela@gmail.com>
2017-05-31 18:51:18 -0400

    Edited main control script

--- icee.m
+++ icee.m
@@ -13,21 +13,20 @@
 % clear the workspace and add the ./functions directory to the MATLAB 
 % search path
 clear
-addpath(genpath([pwd filesep 'functions']))
+addpath(genpath([mfilename('fullpath') filesep 'functions']))
 
 % Ask the user for input
 % DBmode   = Debugging Mode (smaller screen)
 % subject  = subject number
 
-DBmode  = input('Debug mode? y/n: ','s');
-subject = input('Enter subject ID: ','s');
+DBmode   = input('Debug mode? y/n: ','s');
+subject  = input('Enter subject ID: ','s');
 
 % Hard coded yes/no variables:
 % y = yes
 % n = no
 YN.enc  = 'y';              % run encoding
 YN.ret  = 'y';              % run retrieval
-YN.FTB  = 'y';              % run find
 YN.instructAutoSkip = 'n';  % autoskip instruction screens
 
 % Initalize Psychtoolbox
@@ -38,7 +37,7 @@ TimeStamp = [datestr(clock,'yyyy-mm-dd-HHMM') datestr(clock,'ss')];
 
 % if running debug mode, make the experiment go faster
 if strcmp(DBmode, 'y')
-    fast = 1; % .5 = 2x as fast, .1 = 10x as fast, 1 = real time, ect.
+    fast = .5; % .5 = 2x as fast, .1 = 10x as fast, 1 = real time, ect.
 else
     fast = 1;
 end
@@ -48,83 +47,37 @@ end
 try
     %% Try Running Experiment
     
-    % Generate the Encoding, Retrieval, and Find Trial Lists
-    [Encoding, Retrieval, Find] = generate_lists(fullfile(pwd, 'stim'));
+    % Generate the Encoding, Retrieval Trial Lists
+    stimdir               = fullfile(mfilename('fullpath'), 'stim');
+    [Encoding, Retrieval] = generate_lists(stimdir);
     
-    % Preload the trial stimuli prior to beginning the experiment
-    [Encoding, Retrieval, Find, E_pahandle, N_pahandle] = preload_stim(Encoding, Retrieval, Find);    
+    % Preload the Trial Stimuli. This may take a few seconds
+    [Encoding, Retrieval] = preload_stim(Encoding, Retrieval);    
     
-    enccount  = 0;
-    retcount  = 0;
-    findcount = 0;
-    
-    % Each experimental run is paired in the following manner:
-    % Enc1, Enc2, Ret1, Ret2, Find1, Find2
-    % Enc3, Enc4, Ret3, Ret4, Find3, Find4
-    
-    for outer = 1:length(unique(Encoding.RunID))/2
+    % For each run...
+    for crun = 1:nruns
         
         %-- "Welcome to Run" Screen
         
         instructions = ['Welcome to Run ' num2str(outer)];
         directions   = 'Press spacebar when you are ready to continue';
         instructions_screen(instructions, directions, YN.instructAutoSkip);
-        
-        %-- Paired Encoding
-
-        for encinner = 1:2
             
-            % advance enc counter; grab this Encoding run's trials
-            enccount   = enccount + 1;
-            thisEncRun = Encoding(Encoding.RunID == enccount,:);
-            
-            % run encoding phase
-            if strcmp(YN.enc, 'y')              
-                encoding;
-            end
-        
+        % Run Encoding
+        if strcmp(YN.enc, 'y')              
+            encoding;
         end
-        
+ 
         %-- View/Remember Buffer Screen
         
         instructions = 'You are finished with the pair View Rounds';
         directions   = 'Press spacebar when you are ready to continue to the Remember Rounds';
         instructions_screen(instructions, directions, YN.instructAutoSkip);
-        
-        %-- Paired Retrieval
-        
-        for retinner = 1:2
-            
-            % advance ret counter; grab this Retrieval run's trials
-            retcount   = retcount + 1;
-            thisRetRun = Retrieval(Retrieval.RunID == retcount, :);
+       
 
-            % run retrieval phase
-            if strcmp(YN.ret, 'y')
-                retreival;
-            end
-            
-        end
-        
-        %-- Retrieval/Find Buffer Screen
-        
-        instructions = 'You are finished with the pair of Remember Rounds';
-        directions   = 'Press spacebar when you are ready to continue to the Find Rounds';
-        instructions_screen(instructions, directions, YN.instructAutoSkip);
-
-        %-- Paired Find
-        
-        for findinner = 1:2
-            
-            % advance find counter; grab this Find run's trials
-            findcount       = findcount + 1;
-            thisFindTheBoop = Find(Find.RunID == findcount, :);
-
-            % run find phase
-            if strcmp(YN.FTB, 'y')
-                find_the_boop;
-            end
-        
+        %-- Run Retrieval
+        if strcmp(YN.ret, 'y')
+            retreival;
         end
 
     end
@@ -154,7 +107,6 @@ catch
 
     % Close all PTB screens (sca) and show the cursor again (ShowCursor) 
     sca;
-    PsychPortAudio('Close');
     ShowCursor;
     
     % If we are using a PC, show the task bar at the bottom    
