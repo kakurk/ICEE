e94b214614f2b4f286072f5647838777befb033e
kkurkela <kyleakurkela@gmail.com>
2017-06-03 17:53:53 -0400

    updates to preload_stim
    1.) Added stimdir input argument
    2.) Added sorting columns functionality

--- functions/preload_stim.m
+++ functions/preload_stim.m
@@ -10,7 +10,8 @@ function [varargout] = preload_stim(varargin)
 global W
 
 TrialList = varargin{1}; % Generic Trial List
-FNColumn  = varargin{2}; % Column in Table with the File Names
+stimdir   = varargin{2}; % stimuli directory
+FNColumn  = varargin{3}; % Column in Table with the File Names
 
 %%
 %==========================================================================
@@ -20,7 +21,7 @@ FNColumn  = varargin{2}; % Column in Table with the File Names
 % assigning the unique Image ID's to trials that contain that stimuli
 
 % Find all of the unique item stimuli in this study
-uniquestim = unique(TrialList.FNColumn);
+uniquestim = unique(TrialList.(FNColumn));
 
 % Initialize an ItemimID variable for Encoding, Retrieval, and Find Lists
 TrialList.([FNColumn 'id']) = zeros(height(TrialList), 1);
@@ -30,20 +31,30 @@ for stim = uniquestim'
     
     % Create a filter for all of the trials in the Encoding, Retrieval, 
     % and Find lists that contain this item stimuli
-    filter         = strcmp(TrialList.FNColumn, stim{:});
+    filter         = strcmp(TrialList.(FNColumn), stim{:});
     
     % Preload Stimuli
-    im        = imread(stim{:});
+    im        = imread(fullfile(stimdir, stim{:}));
     imID      = Screen('MakeTexture', W, im);
     
     % Assign this image ID to the trials the contain this stimuli in the
     % Encoding and Retrieval lists
     if any(filter)
-        TrialList.FNColumn(filter)  = imID;
+        TrialList.([FNColumn 'id'])(filter)  = imID;
     end
     
 end
-    
+
+% ReArrange the Columns so that the Stim Column and Stimid Columns are next
+% to one another
+columns_names     = TrialList.Properties.VariableNames;
+idx1              = find(strcmp(FNColumn, columns_names));
+idx2              = find(strcmp([FNColumn 'id'], columns_names));
+firstRun          = [1:idx1,idx2];
+secondRun         = setdiff(1:length(columns_names), firstRun);
+ReArrangedColumns = columns_names([firstRun secondRun]);
+TrialList         = TrialList(:,ReArrangedColumns);
+
 %% 
 %==========================================================================
 %                   Expand Out Arguments
