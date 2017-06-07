5ea6921516acc1bdd20af8cecbb986135c94362d
kkurkela <kyleakurkela@gmail.com>
2017-06-07 18:53:44 -0400

    Kyle's edits to the ICEE DM Script
    List of changes:
    
    1.) Changed Study.studyDir to the path on my computer
    2.) Changed the subject ID to 'y106'
    3.) Added a subjDir variable, which tracks the current subject's behavioral directory
    4.) Changed the xlsread functions --> readtable functions
    5.) Changed the parenthesis to curly braces. Important when working with tables
    6.) Made the for loops start from 1 instead of 2. Tables variables don't count headers
    7.) Changed the if statment to use strcmp when identifying targets
    8.) Changed all 'length' functions to 'height'. Table variables don't like the length function
    9.) Changed the initaliziation of the DMscore column in the encoding table. When creating new columns in MATLAB tables, you use the table.newcolumn syntax
    10.) Changed the output file name to .csv. I prefer csv's (they tend to work better with git). You can still read csv's into excel
    11.) Defined path2 variable as the folder where the original encoding file was held

--- ICEE_DMscript.m
+++ ICEE_DMscript.m
@@ -1,51 +1,48 @@
 clear all;
-Study.studyDir = 'R:\DennisLab\Studies\Elon_ICE_Scanner_Summer_2017\ICEE_master\data';
-subj = {'y102'};
+Study.studyDir = '/Users/kylekurkela/Documents/MATLAB/githubrepos/ICEE_Task'; % R:\DennisLab\Studies\Elon_ICE_Scanner_Summer_2017\ICEE_master\data
+subj = {'y106'};
 
 for s = 1:length(subj)
     
-    retfile = strcat(Study.studyDir,filesep, subj{s},filesep,  subj{s}, '_retALL.xls');
-    encfile = strcat(Study.studyDir,filesep, subj{s},filesep,  subj{s}, '_encALL.xls');
-    %[path,name,ext,versn] = fileparts(retfile);
-    %[path2,name2,ext2,versn2] = fileparts(encfile);
-    [num,text,ret] = xlsread(retfile); 
-    [num2,text2,enc] = xlsread(encfile);
+    subjDir = fullfile(Study.studyDir, subj{s});
+    
+    retfile = spm_select('FPList', subjDir, '.*retrieval.*\.csv');
+    encfile = spm_select('FPList', subjDir, '.*encoding.*\.csv');
+
+    ret   = readtable(retfile); 
+    enc   = readtable(encfile);
     
     %Accuracy info from RET file
-    for i = 2:length(ret)
-        test(i)  = {char(ret{i,3})}; %face img
-        on(i)    = ret(i,2);        %type (only care about targets)
-        response(i) = ret(i,14);    %response
+    for i = 1:height(ret)
+        test(i)     = {char(ret{i,3})}; %face img
+        on(i)       = ret{i,2};        %type (only care about targets)
+        response(i) = ret{i,14};    %response
     end
 
     %Image info from ENC file
-    for i = 2:length(enc)
+    for i = 1:height(enc)
         test2(i) = {char(enc{i,4})}; %face img
     end
 
-    for k = 2:length(ret); %ret is RET xls
-        if on{k} == 0 || on{k} == 1 || on{k} == 2 || on{k} == 3 %Should this be where we tell it to pull rows with Target?
+    for k = 1:height(ret); %ret is RET xls
+        if strcmp(on{k}, 'Target') %Should this be where we tell it to pull rows with Target?
             filematch = find(strcmp(test(1,k),test2)==1);  %match on face img
             DMscore(filematch) = response(k); %match on face img
        end
     end
 
-    if length(DMscore) ~= length(enc) %enc is ENC xls
-        DMscore(length(enc)) = {0};
+    if length(DMscore) ~= height(enc) %enc is ENC xls
+        DMscore(height(enc)) = {0};
     end
 
-    enc(:,15) = DMscore;
-    enc(1,15) = {'DMscore'};
+    enc.DMscore = DMscore';
 
-    filename = [strcat(subj{s}) 'enc_DM.xls']
+    filename = [strcat(subj{s}) 'enc_DM.csv']
 
     oldpath=pwd;
+    path2  = fileparts(encfile);
     cd(path2);
-    xlswrite(filename, enc);
+    writetable(enc, filename);
     cd(oldpath);
 
-    %oldpath=pwd;
-    %cd(path2);
-    %xlswrite(strcat(subj(s), 'enc_DM.xls'), enc);
-    %cd(oldpath);
 end
